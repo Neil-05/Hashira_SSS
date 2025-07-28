d99a57fa2c4a76cde8b3307a6a33544bc946570b
Neil Parkhe <neilparkhe@gmail.com>
2025-07-28 12:46:17 +0530

    Updated code with sorted share selection and improvements

--- ShamirSecretSharing.java
+++ ShamirSecretSharing.java
@@ -1,93 +1,103 @@
 import com.google.gson.*;
 import java.io.FileReader;
-import java.io.Reader;
 import java.math.BigInteger;
 import java.util.*;
 
 public class ShamirSecretSharing {
-
-    static class Share {
-        int x;
-        BigInteger y;
-
-        Share(int x, BigInteger y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    // 256-bit prime (close to 2^256)
-    static final BigInteger PRIME = new BigInteger("115792089237316195423570985008687907852837564279074904382605163141518161494337");
-
     public static void main(String[] args) {
         try {
-            Reader reader = new FileReader("input.json");
-            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
-
-            JsonObject keys = jsonObject.getAsJsonObject("keys");
+          
+            JsonObject json = JsonParser.parseReader(new FileReader("input.json")).getAsJsonObject();
+            JsonObject keys = json.getAsJsonObject("keys");
             int n = keys.get("n").getAsInt();
-            int k = keys.get("k").getAsInt();
+            int k = keys.get("k").getAsInt(); 
 
-            List<Share> shares = new ArrayList<>();
+            List<BigInteger> xList = new ArrayList<>();
+            List<BigInteger> yList = new ArrayList<>();
 
-            for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
-                if (entry.getKey().equals("keys")) continue;
+           
+            for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+                String key = entry.getKey();
+                if (key.equals("keys")) continue;
 
-                int x = Integer.parseInt(entry.getKey());
-                JsonObject obj = entry.getValue().getAsJsonObject();
-                int base = Integer.parseInt(obj.get("base").getAsString());
-                String valueStr = obj.get("value").getAsString();
+                int x = Integer.parseInt(key);
+                JsonObject root = entry.getValue().getAsJsonObject();
+                int base = Integer.parseInt(root.get("base").getAsString());
+                String value = root.get("value").getAsString();
+                BigInteger y = new BigInteger(value, base);
 
-                try {
-                    BigInteger y = new BigInteger(valueStr, base);
-                    shares.add(new Share(x, y));
-                } catch (Exception e) {
-                    System.out.println("Invalid value at x = " + x + ". Skipping.");
+                xList.add(BigInteger.valueOf(x));
+                yList.add(y);
+                if (xList.size() == k) break; 
+            }
+
+          
+            BigInteger[][] A = new BigInteger[k][k];
+            BigInteger[] Y = new BigInteger[k];
+
+            for (int i = 0; i < k; i++) {
+                BigInteger xi = xList.get(i);
+                BigInteger power = BigInteger.ONE;
+                for (int j = k - 1; j >= 0; j--) {
+                    A[i][j] = power;
+                    power = power.multiply(xi);
                 }
+                Y[i] = yList.get(i);
             }
 
-            if (shares.size() < k) {
-                System.out.println("Unable to recover secret. Too many invalid shares.");
-                return;
-            }
+            
+            BigInteger[] coeffs = gaussianElimination(A, Y);
 
-            // Pick first k shares (can be randomized/shuffled for robustness)
-            List<Share> selectedShares = shares.subList(0, k);
-            BigInteger secret = lagrangeInterpolation(selectedShares, PRIME);
-            System.out.println("Recovered secret: " + secret);
+         
+            System.out.println("Recovered Secret: " + coeffs[k - 1]);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // Lagrange Interpolation to find secret (i.e., constant term of polynomial)
-    static BigInteger lagrangeInterpolation(List<Share> shares, BigInteger prime) {
-        BigInteger result = BigInteger.ZERO;
+  
+    static BigInteger[] gaussianElimination(BigInteger[][] A, BigInteger[] Y) {
+        int n = A.length;
 
-        for (int i = 0; i < shares.size(); i++) {
-            int xi = shares.get(i).x;
-            BigInteger yi = shares.get(i).y;
-
-            BigInteger numerator = BigInteger.ONE;
-            BigInteger denominator = BigInteger.ONE;
-
-            for (int j = 0; j < shares.size(); j++) {
-                if (i != j) {
-                    int xj = shares.get(j).x;
-                    numerator = numerator.multiply(BigInteger.valueOf(-xj)).mod(prime);
-                    denominator = denominator.multiply(BigInteger.valueOf(xi - xj)).mod(prime);
+        for (int i = 0; i < n; i++) {
+            
+            int maxRow = i;
+            for (int k = i + 1; k < n; k++) {
+                if (A[k][i].abs().compareTo(A[maxRow][i].abs()) > 0) {
+                    maxRow = k;
                 }
             }
 
-            BigInteger term = yi.multiply(numerator).multiply(modInverse(denominator, prime)).mod(prime);
-            result = result.add(term).mod(prime);
+         
+            BigInteger[] tempRow = A[i];
+            A[i] = A[maxRow];
+            A[maxRow] = tempRow;
+
+            BigInteger tempY = Y[i];
+            Y[i] = Y[maxRow];
+            Y[maxRow] = tempY;
+
+           
+            for (int k = i + 1; k < n; k++) {
+                BigInteger factor = A[k][i].divide(A[i][i]);
+                for (int j = i; j < n; j++) {
+                    A[k][j] = A[k][j].subtract(factor.multiply(A[i][j]));
+                }
+                Y[k] = Y[k].subtract(factor.multiply(Y[i]));
+            }
+        }
+
+      
+        BigInteger[] result = new BigInteger[n];
+        for (int i = n - 1; i >= 0; i--) {
+            BigInteger sum = Y[i];
+            for (int j = i + 1; j < n; j++) {
+                sum = sum.subtract(A[i][j].multiply(result[j]));
+            }
+            result[i] = sum.divide(A[i][i]);
         }
 
         return result;
-    }
-
-    static BigInteger modInverse(BigInteger a, BigInteger mod) {
-        return a.modInverse(mod);
     }
 }
